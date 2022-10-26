bee5a7d5b73e14b1681126ffd1f670f86b9ec483
barbero kylian <brk4443a@ens.fsi>
2022-10-26 14:29:06 +0200

    TP4.0 - Classe Humain créée

---
+++ src/personnages/Humain.java
@@ -0,0 +1,62 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonPref;
+	private int argent;
+	
+	public Humain(String nom, String boissonPref, int argent) {
+		this.nom = nom;
+		this.boissonPref = boissonPref;
+		this.argent = argent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getBoissonPref() {
+		return boissonPref;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+	
+	private void parler(String texte) {
+		System.out.println('(' + nom + ") - " + texte);
+	}
+	
+	public void boire(String boisson) {
+		parler("Mmmm, un bon verre de " + boisson + " ! GLOUPS !");
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m’appelle " + nom 
+					+ " et j’aime boire du " + boissonPref + '.');
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (argent >= prix) {
+			parler("J'ai " + argent + " sous en poche. Je vais "
+					+ "pouvoir m'offrir " + bien + " à " + prix + " sous.");
+			perdreArgent(prix);
+		} else {
+			parler("Je n'ai plus que " + argent +" sous en poche."
+					+ " Je ne peux même pas m'offrir " + bien + " à "
+					+ prix + " sous.");
+		}
+	}
+	
+	public void gagnerArgent(int gain) {
+		argent += gain;
+	}
+
+	public void perdreArgent(int perte) {
+		argent -= perte;
+		if (argent<0) {
+			argent = 0;
+		}
+	}
+
+}
